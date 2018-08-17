1d06e259e3fd42e9936142ce7b2460d6bd109bb4
RodolfoHerman <ciencia.rodolfo@gmail.com>
2018-08-17 20:52:10 -0300

    Tratando o CSS para Conta Component

--- src/app/financas/conta/conta.component.ts
+++ src/app/financas/conta/conta.component.ts
@@ -7,12 +7,53 @@ import { Conta } from './conta.model';
 })
 export class ContaComponent implements OnInit {
 
-  
+
   @Input() conta: Conta;
-  
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getIcon(): string {
+
+    let temp = this.conta.descricao.toLocaleLowerCase();
+    let icon: string;
+
+    switch (temp) {
+      
+      case 'luz':
+        
+        icon = 'fa-bolt';
+      break;
+      
+      case 'água' || 'agua':
+
+        icon = 'fa-tint';
+      break;
+
+      case 'telefone':
+
+        icon = 'fa-phone';
+      break;
+
+      case 'consórcio' || 'consorcio':
+
+        icon = 'fa-car';
+      break;
+
+      case 'compra' || 'compras' || 'mercado':
+
+        icon = 'fa-cart-arrow-down';
+      break;
+
+      default:
+
+        icon = 'fa-money';
+      break;
+    }
+
+    return icon;
   }
 
 }
